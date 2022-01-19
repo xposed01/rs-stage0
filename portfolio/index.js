67fabb5ef626929686f8251a74a051ba5a271187
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-01-19 18:15:26 +0700

    feat: add burger menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,3 +1,30 @@
-console.log('Вёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48');
-console.log('Требования к css + 12\nИнтерактивность, реализуемая через css +20');
-console.log(`Итого: ${10+20+48+12+20} баллов`); 
+
+// burger-animation
+
+(function () {
+  const burgerButton = document.querySelector('.menu-burger');
+  const burgerMenuOpen = document.querySelector('.navigation');
+  const bodyBlackout = document.querySelector('.blackout');
+  const burgerMenuNavClose = document.querySelector('.navigation-list') 
+  // blackout burger on/off
+  burgerButton.addEventListener('click', () => {
+  document.body.classList.toggle('_lock');
+  burgerButton.classList.toggle('_active');
+  burgerMenuOpen.classList.toggle('navigation-active');
+  bodyBlackout.classList.toggle('blackout-active');
+  });
+  // blackout ouside click off
+  bodyBlackout.addEventListener('click', () => {
+  document.body.classList.remove('_lock');
+  burgerButton.classList.toggle('_active');
+  burgerMenuOpen.classList.remove('navigation-active');
+  bodyBlackout.classList.remove('blackout-active');
+  });
+  // blackout menu click off
+  burgerMenuNavClose.addEventListener('click', () => {
+  document.body.classList.remove('_lock');
+  burgerButton.classList.toggle('_active');
+  burgerMenuOpen.classList.remove('navigation-active');
+  bodyBlackout.classList.remove('blackout-active');
+  });
+}());
