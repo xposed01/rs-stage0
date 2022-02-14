65fcc5b3f30a9912070d4547cd8b453bfdd9dd14
xposed01 <43813249+xposed01@users.noreply.github.com>
2022-02-14 22:17:26 +0700

    add: background animation

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -1,16 +1,23 @@
 const url = 'https://type.fit/api/quotes';
 
-let generatedText = document.querySelector('.generated-text');
-
+const generatedText = document.querySelector('.generated-text');
+const image = document.querySelector('.main-image');
+const body = document.querySelector('body');
 
 // random generator
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// bacground animation
+function backgroundAnimation() {
+  body.style.backgroundImage = `linear-gradient(${getRandom(0, 555)}deg, #add1e9 0%, #b3b5df 100%)`;
+}
+
 // API show
 function showData(data) {
   generatedText.innerText = data[getRandom(0, data.length-1)].text;
+  backgroundAnimation();
 }
 
 // API async
